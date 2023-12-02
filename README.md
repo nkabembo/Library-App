0dd74f7e45fbef9f671b9370cf9328e8459b0b77
Naghmeh K <naghmehmitonga99@gmail.com>
2023-12-01 21:38:13 -0500

    add Remaining Functionality

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Library-App
+
+![Alt text](image.png)
+
+- Add Book Object
+  -Toggle Book Status from Read to Not Read (after clicking tag)
+  -Edit a book
+  -Delete a book
+  -Delete all books
+  -Filter books(All, Read and Not Read)
--- app.js
+++ app.js
@@ -1,20 +1,48 @@
+
+let myLibrary = [];//Array storing Book Objects
+if (localStorage.getItem('books') === null) {
+    myLibrary = [];
+} else {
+    const booksFromStorage = JSON.parse(localStorage.getItem('books'));
+    myLibrary = booksFromStorage;
+}
+
+function Book(title, author , pages, read){
+    this.read = read;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.info = function (){
+        return `${this.title} by ${this.author}, ${this.pages} pages,
+        , ${this.read}`;
+    }
+}
+//Book Prototype
+Book.prototype.toggleStatus = function(){
+    if ( this.read === 'read'){
+        this.read = 'not read'
+    }else{
+        this.read = 'read';
+    }
+}
 //Total book info display
 let total_books = document.querySelector('.total-books');
 let unread_books = document.querySelector('.unread-books');
 let read_books = document.querySelector('.read-books');
 
-
 //update book info display
 function bookInfo(){
     total_books.textContent = myLibrary.length;
-    let read_total,unread_total = 0;
-    myLibrary.forEach(libraryItem => {
-        if(libraryItem.read === "read"){
-            read_total++;
-        } else {
-            unread_total++;
-        }
-    });
+    let read_total = 0,unread_total = 0;
+    if(myLibrary.length > 0){
+        myLibrary.forEach(libraryItem => {
+            if(libraryItem.read === "read"){
+                read_total++;
+            } else if(libraryItem.read === "not read") {
+                unread_total++;
+            }
+        });
+    }
     unread_books.textContent = unread_total;
     read_books.textContent = read_total;
 }
@@ -33,13 +61,12 @@ function DropDownToggle(){
 }
 //Add Form inputs
 let addBtn = document.getElementById('add');
-let filter = document.getElementById('filter');
+let filter = document.getElementById('sort');
 
 
 const deleteDialog = document.getElementById("delete-dialog");
 const editDialog = document.getElementById("edit-dialog");
 const deleteAllBtn = document.getElementById("delete-all");
-const editBtn = document.querySelector('.fa-pen-to-square');
 const deleteBtn = document.querySelector(".delete");
 const cancelBtn = document.querySelector(".cancel");
 deleteAllBtn.addEventListener('click', () => {
@@ -47,11 +74,14 @@ deleteAllBtn.addEventListener('click', () => {
 });
 
 cancelBtn.addEventListener("click", () => {
+    displayBooks();
+    bookInfo();
+    console.log("cancel")
     deleteDialog.close();
+    
 });
 deleteBtn.addEventListener("click", () => {
     deleteBooks();
-    deleteDialog.close(); 
 });
 
 
@@ -59,26 +89,13 @@ deleteBtn.addEventListener("click", () => {
 function deleteBooks(){
     myLibrary.length = 0;
     localStorage.clear();
+    deleteDialog.close(); 
+    bookInfo();
+    displayBooks();
 }
-editBtn.addEventListener('click', () => {
-    editDialog.showModal();
-});
 
-
-let myLibrary = [];//Array storing Book Objects
-function Book(title, author , pages, read){
-    this.read = read;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.info = function (){
-        return `${this.title} by ${this.author}, ${this.pages} pages,
-        , ${this.read}`;
-    }
-}
 
 addBtn.addEventListener('click', getBookInfo);
-//parseInt(e.toElement.attributes.librarybookid.value);
 function getBookInfo(e){
     e.preventDefault();
     let title_input = document.getElementById('title').value;
@@ -93,7 +110,6 @@ function getBookInfo(e){
     addBookToLibrary(title_input, author_input, pageCount_input, readStatus_input);
     //Clear form
     document.querySelector('.add-form').reset();
-    displayBooks();
     console.log(myLibrary);
 }
 
@@ -101,18 +117,20 @@ function getBookInfo(e){
 function addBookToLibrary(title, author , pages, read){
     let book = new Book(title, author, pages, read);
     myLibrary.push(book);
-    updateLocalStorage();
+    updateLocalStorage(myLibrary);
     displayBooks();
     bookInfo();
-    //localStorage.setItem('books', JSON.stringify(books));
 }
-function updateLocalStorage(){
-    localStorage.setItem('books', JSON.stringify(myLibrary));
+
+function updateLocalStorage(book){
+    localStorage.setItem('books', JSON.stringify(book));
 }
+
 let table = document.getElementById('table-display');
 //Display Books from Book array into table
 function displayBooks(){
     let index = 0;
+    table.textContent="";
     myLibrary.forEach(book =>{
         let row = document.createElement("tr");
         table.appendChild(row);
@@ -160,50 +178,189 @@ function displayBooks(){
 }
 
 
+table.addEventListener('click',function(e){
+    if(e.target.classList.contains('fa-pen-to-square')){
+        let rowIndex = e.target.dataset.editIndex;
+        console.log('edit button');
+        console.log(e.target);
+        const edit = document.querySelector(".edit");
+        document.getElementById('edit-title').value = myLibrary[rowIndex].title;
+        document.getElementById('edit-author').value = myLibrary[rowIndex].author;
+        document.getElementById('edit-pages').value = myLibrary[rowIndex].pages;
+        document.getElementById('edit-status').value = myLibrary[rowIndex].read;
+        editDialog.showModal();
+        
+        let cancel_edit_Btn = document.querySelector(".cancel-edit");
+        cancel_edit_Btn.addEventListener('click', ()=>{
+            editDialog.close();
+        });
+        edit.addEventListener('click', (e)=>{
+            e.preventDefault();
+            let title_edit_input = document.getElementById('edit-title').value;
+            let author_edit_input = document.getElementById('edit-author').value;
+            let pageCount_edit_input = document.getElementById('edit-pages').value;
+            let readStatus_edit_input = document.getElementById('edit-status').value;
+            myLibrary[rowIndex].title = title_edit_input;
+            //myLibrary[rowIndex].title =  document.getElementById('edit-title').value;
+            console.log("Value in title" + document.getElementById('edit-title').value);
+            console.log('the title ..........'+ myLibrary[rowIndex].title);
+            myLibrary[rowIndex].author = author_edit_input;
+            myLibrary[rowIndex].pages = pageCount_edit_input;
+            myLibrary[rowIndex].read = readStatus_edit_input;
+            console.log("My Library after edit" + JSON.stringify(myLibrary[0]));
+            updateLocalStorage(myLibrary);
+            displayBooks();
+            bookInfo();
+            editDialog.close();
+        });
+        displayBooks();
 
-//create book object , create book item with index to indentify item from table , append that to tab;e 
-//create each book colum html element adding content and appropriate class; book item is a single book
-//-- set attribute class 
-//splice , map save and  render
+        
+    }else if(e.target.classList.contains('fa-trash')){
+        console.log('delete button');
+        console.log(e.target);
+        let rowIndex = e.target.dataset.removeIndex;
+        //table.deleteRow(rowIndex);
+        myLibrary.splice(rowIndex,1);
+        updateLocalStorage(myLibrary);
+        displayBooks();
+        //update local storage
+        //update book info
+        bookInfo();
+    }else if (e.target.classList.contains('read-status') ||e.target.classList.contains('not-read-status') ){
+        console.log('status');
+        let bookIndex = (e.target.parentNode.parentNode.rowIndex - 1);
+        console.log(bookIndex);
+        toggleBookReadStatus(bookIndex);
+    }
+});
+//Filtering Display by Sort Criteria
+filter.addEventListener('change', function(){
+    let selected = filter.selectedIndex;
+
+    if(filter.options[selected].value === 'read'){
+        console.log(' READ');
+        let index = 0;
+        table.textContent = '';
+        myLibrary.forEach(libraryItem => {
+            if(libraryItem.read === "read"){
+                let row = document.createElement("tr");
+                table.appendChild(row);
+                
+                //Adding Data Columns to the Table
+                let bookTitle = document.createElement('td');
+                bookTitle.textContent = libraryItem.title;
+                row.appendChild(bookTitle);
+
+                let bookAuthor = document.createElement('td');
+                bookAuthor.textContent = libraryItem.author;
+                row.appendChild(bookAuthor);
+
+                let bookPages = document.createElement('td');
+                bookPages.textContent = libraryItem.pages;
+                bookPages.classList.add('page');
+                row.appendChild(bookPages);
+
+                let bookStatus = document.createElement('td');
+                let statusDiv = document.createElement('div');
+                statusDiv.classList.add('read-status');
+    
+                
+                statusDiv.textContent = libraryItem.read;
+                bookStatus.appendChild(statusDiv);
+                row.appendChild(bookStatus);
+
+                let bookEditButton = document.createElement('td');
+                let editingBtn = document.createElement('i'); 
+                editingBtn.classList.add('fa-regular', 'fa-pen-to-square');
+                editingBtn.dataset.editIndex = index;
+                bookEditButton.appendChild(editingBtn);
+                row.appendChild(bookEditButton);
+
+                let bookRemoveButton = document.createElement('td');
+                let removalBtn = document.createElement('i'); 
+                removalBtn.classList.add('fa-solid', 'fa-trash');
+                removalBtn.dataset.removeIndex = index;
+                bookRemoveButton.appendChild(removalBtn);
+                row.appendChild(bookRemoveButton);
+                index++;
+            }
+        });
+    }else if (filter.options[selected].value === 'not read'){
+        console.log('NOT READ');
+       // let index = 0;
+        table.textContent = '';
+        myLibrary.forEach((libraryItem, index) => {
+            if(libraryItem.read === "not read"){
+                        let row = document.createElement("tr");
+                        table.appendChild(row);
+                        console.log('index is ' + index);
+                        //Adding Data Columns to the Table
+                        let bookTitle = document.createElement('td');
+                        bookTitle.textContent = libraryItem.title;
+                        row.appendChild(bookTitle);
+        
+                        let bookAuthor = document.createElement('td');
+                        bookAuthor.textContent = libraryItem.author;
+                        row.appendChild(bookAuthor);
+        
+                        let bookPages = document.createElement('td');
+                        bookPages.textContent = libraryItem.pages;
+                        bookPages.classList.add('page');
+                        row.appendChild(bookPages);
+        
+                        let bookStatus = document.createElement('td');
+                        let statusDiv = document.createElement('div');
+                        statusDiv.classList.add('not-read-status');
+            
+                        
+                        statusDiv.textContent = libraryItem.read;
+                        bookStatus.appendChild(statusDiv);
+                        row.appendChild(bookStatus);
+        
+                        let bookEditButton = document.createElement('td');
+                        let editingBtn = document.createElement('i'); 
+                        editingBtn.classList.add('fa-regular', 'fa-pen-to-square');
+                        editingBtn.dataset.editIndex = index;
+                        bookEditButton.appendChild(editingBtn);
+                        row.appendChild(bookEditButton);
+        
+                        let bookRemoveButton = document.createElement('td');
+                        let removalBtn = document.createElement('i'); 
+                        removalBtn.classList.add('fa-solid', 'fa-trash');
+                        removalBtn.dataset.removeIndex = index;
+                        bookRemoveButton.appendChild(removalBtn);
+                        row.appendChild(bookRemoveButton);
+                        //index++;
+                    }
+        });
+    }else{
+        displayBooks();
+        bookInfo();
+    }
+});
 
 
+//Toggles book object read status
+function toggleBookReadStatus(i){
+    //Creates an empty Book object
+    let target =  Object.create(Book.prototype);
+    console.log( Object.create(Book.prototype) instanceof Book);
+    console.log("THis is target" + JSON.stringify(target));
+    //myLibrary[i] is type [object Object]
+    let source = myLibrary[i];
+    //moving empty [Object object] to Book object (target)
+    const returnedTarget = Object.assign(target, source);
+    console.log("trying to toggle");
+    console.log("THis is target after" + JSON.stringify(target));
 
-
-/*
-Toggles book object read status
-*/
-/*function ToggleBookReadStatus(){
-
+    console.log("Returned target after" + JSON.stringify(returnedTarget));
+    target.toggleStatus();
+    console.log("Returned target after ater ................" + JSON.stringify(target));
+    myLibrary[i] = target;
+    displayBooks();
+    bookInfo();
 }
+bookInfo();
+displayBooks();
 
-/*
-Edit book information
-*/
-/*function EditBook(){
-    updateLocalStorage();
-}
-
-/*
-Delete all Books
-*/
-/*function deleteBooks(){
-    myLibrary.length = 0;
-}
-function filterBooks(){
-
-}
-/*
-Deletes a book
-*/
-/*function RemoveBook(){
-
-
-}
-let statusBtnId = e.target.id;
-  let removeBtnId = e.target.dataset.id;
-  let index;
- 
-
-  // change the text content and class of the button
-  if (e.target.className === 'far fa-times-circle') {
-*/
